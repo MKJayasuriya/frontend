b3fda9a7742b87e4a387042b76de66489cb34911
MKJayasuriya <mkjayasuriya17042001@gmail.com>
2025-03-24 21:56:40 +0530

    Added Day 8: Introduction to TypeScript

---
+++ Day8/main.js
@@ -0,0 +1,4 @@
+var message = "Hello, TypeScript!";
+var age = 23;
+var isStudent = true;
+console.log(message, age, isStudent);
---
+++ Day8/main.ts
@@ -0,0 +1,5 @@
+let message: string = "Hello, TypeScript!";
+let age: number = 23;
+const isStudent: boolean = true;
+
+console.log(message, age, isStudent);
