2fa13ace10f1f5a8a4f648128db207714771529a
MKJayasuriya <mkjayasuriya17042001@gmail.com>
2025-03-26 21:50:52 +0530

    Added Day 10 TypeScript Objects & Interfaces

---
+++ Day10/main.js
@@ -0,0 +1,7 @@
+// Example usage
+var user = {
+    name: "John Doe",
+    age: 25,
+    email: "john.doe@example.com",
+};
+console.log(user);
---
+++ Day10/main.ts
@@ -0,0 +1,14 @@
+interface User {
+    name: string;
+    age: number;
+    email: string;
+}
+
+// Example usage
+const user: User = {
+    name: "John Doe",
+    age: 25,
+    email: "john.doe@example.com",
+};
+
+console.log(user);
